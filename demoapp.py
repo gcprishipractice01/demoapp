5d82e411391b87eb00e7134c2bb3a09a673bf320
Vaibhav Gawali <vaibhavgawali@example.com>
2019-07-02 22:56:57 +0530

    Updated demoapp file

--- demoapp.py
+++ demoapp.py
@@ -3,8 +3,17 @@ import socket
 demoapp = Flask(__name__)
 
 @demoapp.route('/')
+def welcome():
+    return 'Welcome to Demo app'
+
+@demoapp.route('/hostname')
 def hostname():
     return socket.gethostname()
 
+@demoapp.route('/status')
+def status():
+    return 'App is running'
+
 if __name__ == '__main__':
-    demoapp.run()
+    demoapp.run(host="0.0.0.0", port=80)
+    
